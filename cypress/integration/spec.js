d8ed85cb474fbb67fa760b237d0477608b147fce
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-23 14:32:17 -0500

    remove comments from spec

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,7 +2,6 @@
 it('works', () => {
   cy.visit('/', {
     onBeforeLoad(win) {
-      // delete win.navigator.__proto__.geolocation
       const err = new Error('User denied')
       err.code = GeolocationPositionError.PERMISSION_DENIED
       cy.stub(win.navigator.geolocation, 'getCurrentPosition').callsArgWith(0, err)
